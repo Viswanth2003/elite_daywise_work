26d05de5593573c00439c05baa4d81c9f4a7f9bf
Viswanth2003 <84609791+Viswanth2003@users.noreply.github.com>
2023-01-29 12:20:04 +0530

    Add files via upload

---
+++ Minimum distance between BST nodes.java
@@ -0,0 +1,21 @@
+class Solution {
+    List<Integer>lt=new ArrayList<>();
+    public void inordertraversal(TreeNode root){
+        if(root!=null){
+        inordertraversal(root.left);
+        lt.add(root.val);
+        inordertraversal(root.right);
+        }
+    }
+    public int minDiffInBST(TreeNode root) {
+         inordertraversal(root);
+        int min=10000000;
+        for(int i=0;i<lt.size()-1;i++){
+            int a=Math.abs(lt.get(i)-lt.get(i+1));
+            if(a<min){
+                min=a;
+            }
+        }
+        return min;
+    }
+}
